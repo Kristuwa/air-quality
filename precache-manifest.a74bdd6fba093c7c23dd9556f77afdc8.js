49b999fcce9ee9ea8889f861e77fc2615ee6a40a
Kristuwa <kristuwa15@ukr.net>
2020-10-01 20:15:02 +0300

    Updates

---
+++ precache-manifest.a74bdd6fba093c7c23dd9556f77afdc8.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a095ad6ef27153327b1a080a4ae198aa",
+    "url": "/master/index.html"
+  },
+  {
+    "revision": "ac61ef5152aee5d7e55f",
+    "url": "/master/static/css/2.e0686c58.chunk.css"
+  },
+  {
+    "revision": "462fefd36af86a26b128",
+    "url": "/master/static/css/main.a3f9c5e4.chunk.css"
+  },
+  {
+    "revision": "ac61ef5152aee5d7e55f",
+    "url": "/master/static/js/2.a8513620.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/master/static/js/2.a8513620.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "462fefd36af86a26b128",
+    "url": "/master/static/js/main.f0218174.chunk.js"
+  },
+  {
+    "revision": "4b5ae3fa2dc8e872a18f",
+    "url": "/master/static/js/runtime-main.064f7587.js"
+  },
+  {
+    "revision": "2f311a05a79fb86cee6148d658aa8336",
+    "url": "/master/static/media/ITN_mix1.2f311a05.svg"
+  },
+  {
+    "revision": "a3fe3aa17418ceb9f8e4c1961faf77e0",
+    "url": "/master/static/media/city_planing.a3fe3aa1.jpg"
+  },
+  {
+    "revision": "1d9ae9e91593976f92150a639bbdf321",
+    "url": "/master/static/media/energetics.1d9ae9e9.jpg"
+  },
+  {
+    "revision": "17b056bde9ed2137fe8aec437de3fec4",
+    "url": "/master/static/media/energy.17b056bd.jpg"
+  },
+  {
+    "revision": "b236af76d3e12b9f2dc0a2d673375d86",
+    "url": "/master/static/media/heroBG.b236af76.jpg"
+  },
+  {
+    "revision": "3d66b58c57513e79022af1cbb2936e5f",
+    "url": "/master/static/media/industry.3d66b58c.jpg"
+  },
+  {
+    "revision": "341a487c2bb1cbed9179faa73d89b343",
+    "url": "/master/static/media/shid_logo.341a487c.svg"
+  },
+  {
+    "revision": "7e1db0da3d25263be6534e000b29d2a8",
+    "url": "/master/static/media/slide-1.7e1db0da.jpg"
+  },
+  {
+    "revision": "12a25e248f105a327021b69336c5b71b",
+    "url": "/master/static/media/slide-2.12a25e24.jpg"
+  },
+  {
+    "revision": "638ec2275a294137e028e6bce58ad768",
+    "url": "/master/static/media/slide-3.638ec227.jpg"
+  },
+  {
+    "revision": "8a21e8b7fc0908ab482cc0bb0c98138a",
+    "url": "/master/static/media/slide-4.8a21e8b7.jpg"
+  },
+  {
+    "revision": "bb9f49bba9c380e18e77de4c1ed82bf6",
+    "url": "/master/static/media/slide-5.bb9f49bb.jpg"
+  },
+  {
+    "revision": "fb06d4dd85a3187e7d299e96bd5393b8",
+    "url": "/master/static/media/transport.fb06d4dd.jpg"
+  },
+  {
+    "revision": "fd744a5503127e0192d8eb203548db36",
+    "url": "/master/static/media/utilisation.fd744a55.jpg"
+  }
+]);
